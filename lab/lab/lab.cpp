dfcd519dc1e9adc13b344648c47d3ac643fc93b3
Eduardo Guevara <guevarafallas09@mail.com>
2025-11-12 23:18:15 -0600

    Square

--- lab/lab/lab.cpp
+++ lab/lab/lab.cpp
@@ -3,16 +3,16 @@
 #include <vector>
 #include "Shape.h"
 #include "Circle.h"
-//#include "Square.h"
+#include "Square.h"
 
 int main()
 {
     std::vector<std::unique_ptr<Shape>> shapes;
 
     shapes.push_back(std::make_unique<Circle>(5.0));
-   // shapes.push_back(std::make_unique<Square>(4.0));
-    //shapes.push_back(std::make_unique<Square>(3.0));
-   // shapes.push_back(std::make_unique<Square>(2.0));
+    shapes.push_back(std::make_unique<Square>(4.0));
+    shapes.push_back(std::make_unique<Square>(3.0));
+    shapes.push_back(std::make_unique<Square>(2.0));
     shapes.push_back(std::make_unique<Circle>(10.0));
 
     for (const auto& shape : shapes) {
